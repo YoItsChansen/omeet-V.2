98e192f4fcdceff23acc7a90811253a48715a6ee
YoItsChansen <73456882+YoItsChansen@users.noreply.github.com>
2021-08-29 22:00:02 +0800

    dwadaw

--- src/js/server.js
+++ src/js/server.js
@@ -3,6 +3,7 @@ const io = require('socket.io')(3000, {cors: {origin: "*"}});
 const users = {}
 
 io.on('connection', socket => {
+    console.log("It worked")
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', message.user+": "+message.Message)
     })
