a8ad00bf4ae98a37f30085a4a2dcbdd8a6840ed7
YoItsChansen <73456882+YoItsChansen@users.noreply.github.com>
2021-08-29 21:30:12 +0800

    Website stuff

---
+++ src/js/client.js
@@ -0,0 +1,35 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageSpace = document.getElementById('chat-container')
+const messageForm = document.getElementById('sent-container')
+const messageInput = document.getElementById('message-input')
+
+const usrName = prompt('Enter Nickname:')
+
+appendMessage("You joined")
+
+socket.emit('new-user', usrName)
+
+socket.on('chat-message', data => {
+    appendMessage(data)
+})
+
+socket.on('user-connected', data=>{
+    appendMessage(data + " connected")
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    socket.emit('send-chat-message', {Message: message, user:usrName})
+    messageInput.value = ''
+    appendMessage("You: " + message)
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div')
+    messageElement.style.cssText = "font-size: 30%; margin-top: 1%;"
+    messageElement.innerText = message
+    messageSpace.append(messageElement)
+    messageElement.scrollIntoView()
+}
---
+++ src/js/server.js
@@ -0,0 +1,19 @@
+const io = require('socket.io')(3000, {cors: {origin: "*"}});
+
+const users = {}
+
+io.on('connection', socket => {
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message.user+": "+message.Message)
+    })
+    socket.on('new-user', usrName =>{
+        users[socket.id] = usrName
+        socket.broadcast.emit('user-connected', usrName)
+    })
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageSpace.append(messageElement)
+}
