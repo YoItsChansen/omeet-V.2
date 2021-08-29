e5c73e448763a11181ff7cc12899605d55d82dc7
YoItsChansen <73456882+YoItsChansen@users.noreply.github.com>
2021-08-29 22:26:46 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io.connect()
 const messageContainer = document.getElementById('message-container')
 const messageSpace = document.getElementById('chat-container')
 const messageForm = document.getElementById('sent-container')
